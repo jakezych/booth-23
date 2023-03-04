90ac81dd30637e1d09cc7ecfb91cedb2b318993f
victoralfonzo <victoraalfonzo@gmail.com>
2023-03-04 16:30:52 -0500

    improved movement + snap to grid

--- game.py
+++ game.py
@@ -1,77 +1,25 @@
 import pygame 
-import pytmx
 import sys
 import constants
-
-class Player(pygame.sprite.Sprite):
-  def __init__(self, color, width, height):
-    pygame.sprite.Sprite.__init__(self)
-
-    self.color = color
-    self.width = width
-    self.height = height
-
-    self.x = 0
-    self.y = 0 
-
-    self.image = pygame.Surface([width, height])
-    self.image.fill(color)
-    self.rect = self.image.get_rect()
-    pygame.draw.rect(self.image, color, [self.x, self.y, width, height])
-
-    self.x_move = 0
-    self.y_move = 0
-
-
-  def update(self):
-    self.keys()
-    self.move()
-
-  def keys(self):
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-      self.y_move = 1
-    if keys[pygame.K_a]:
-      self.x_move = -1
-    if keys[pygame.K_s]:
-      self.y_move = -1
-    if keys[pygame.K_d]:
-      self.x_move = 1
-    
-  def move(self):
-    self.x += self.x_move
-    self.y += self.y_move
-    self.rect.x = self.x
-    self.rect.y = self.y
-
-
-
-
+import sprites
 
 clock = pygame.time.Clock()
 
 def main():
   pygame.init()
-  # TODO: figure out resolution 
-  size = width, height = 320, 240
-  black = (0, 0, 0)
-
+  size = width, height = constants.WIDTH, constants.HEIGHT
   screen = pygame.display.set_mode(size)
   # TODO: consider setting up something like this: https://github.com/Mekire/cabbages-and-kings/tree/master/data
-  player = Player(constants.WHITE,50,50)
-  
-  
-  
+  player = sprites.Player(constants.WHITE,16,16)
   while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
           sys.exit()
 
-
     player.update()
-    screen.blit(player.image, (player.x, player.y))
-    screen.fill(black)
-    clock.tick(constants.FPS)/1000
+    screen.fill(constants.BLACK)
+    screen.blit(player.image,(player.grid_x, player.grid_y))
+    clock.tick(constants.FPS)
     pygame.display.flip()
 
 
---
+++ sprites.py
@@ -0,0 +1,34 @@
+import pygame
+import constants
+
+class Player(pygame.sprite.Sprite):
+  def __init__(self, color, width, height):
+    pygame.sprite.Sprite.__init__(self)
+
+    self.color = color
+    self.width = width
+    self.height = height
+
+    self.image = pygame.Surface([width, height])
+    self.image.fill(color)
+    self.rect = self.image.get_rect()
+
+    self.grid_x = 0 
+    self.grid_y = 0
+
+    pygame.draw.rect(self.image, color, [self.grid_x, self.grid_y, width, height])
+
+
+  def update(self):
+    self.keys()
+
+  def keys(self):
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+      self.grid_y += -16
+    elif keys[pygame.K_a]:
+      self.grid_x += -16
+    elif keys[pygame.K_s]:
+      self.grid_y += 16
+    elif keys[pygame.K_d]:
+      self.grid_x += 16
