32d571ec36817e972d1db196a6563df4ab69cad2
victoralfonzo <victoraalfonzo@gmail.com>
2023-03-15 00:52:34 -0400

    pep8 formatting

--- constants.py
+++ constants.py
@@ -8,19 +8,19 @@ FPS = 24
 GRIDSIZE = 16
 SCREEN_SCALING_FACTOR = 3
 
-BLACK = (0,0,0)
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-WHITE = (255,255,255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
 
 obstacles = pygame.sprite.Group()
 
 DEATH_EVENT = pygame.event.custom_type()
 
-class Direction(Enum):
-  UP = 1
-  RIGHT = 2
-  LEFT = 3
-  DOWN = 4
 
+class Direction(Enum):
+    UP = 1
+    RIGHT = 2
+    LEFT = 3
+    DOWN = 4
--- tilemap.py
+++ tilemap.py
@@ -1,6 +1,8 @@
 import pytmx
-import pygame 
+import pygame
 from constants import WIDTH, HEIGHT
+
+
 class TiledMap:
     def __init__(self, filename):
         tm = pytmx.util_pygame.load_pygame(filename, pixelalpha=True)
@@ -17,27 +19,27 @@ class TiledMap:
                     if tile:
                         surface.blit(tile, (x * self.tmxdata.tilewidth,
                                             y * self.tmxdata.tileheight))
-                    
-    def render_top(self, surface, filter='treetop'):
-            ti = self.tmxdata.get_tile_image_by_gid
-            for layer in self.tmxdata.visible_layers:
-                if isinstance(layer, pytmx.TiledTileLayer) and layer.name == filter:
-                    for x, y, gid, in layer:
-                        tile = ti(gid)
-                        if tile:
-                            surface.blit(tile, (x * self.tmxdata.tilewidth,
-                                                y * self.tmxdata.tileheight))
 
+    def render_top(self, surface, filter='treetop'):
+        ti = self.tmxdata.get_tile_image_by_gid
+        for layer in self.tmxdata.visible_layers:
+            if isinstance(layer, pytmx.TiledTileLayer) and layer.name == filter:
+                for x, y, gid, in layer:
+                    tile = ti(gid)
+                    if tile:
+                        surface.blit(tile, (x * self.tmxdata.tilewidth,
+                                            y * self.tmxdata.tileheight))
 
     def make_map(self):
         temp_surface = pygame.Surface((self.width, self.height))
         self.render(temp_surface)
         return temp_surface
-    
+
     def make_map_top(self):
         temp_surface = pygame.Surface((self.width, self.height))
         self.render_top(temp_surface)
         return temp_surface
+
 
 class Camera:
     def __init__(self, width, height):
@@ -49,16 +51,16 @@ class Camera:
         return entity.rect.move(self.camera.topleft)
 
     def apply_offset(self, entity, x_off, y_off):
-        x,y = self.camera.topleft
+        x, y = self.camera.topleft
         x += x_off
         y += y_off
-        return entity.rect.move((x,y))
+        return entity.rect.move((x, y))
 
     def apply_rect(self, rect):
         return rect.move(self.camera.topleft)
 
     def update(self, target):
-        #set x and y position of camera
+        # set x and y position of camera
         #print(target.rect.centerx," ", int(WIDTH / 2))
         x = -target.rect.centerx + int(WIDTH / 2)
         y = -target.rect.centery + int(HEIGHT / 2)
@@ -68,7 +70,8 @@ class Camera:
         y = min(0, y)  # top
         x = max(-(self.width - WIDTH), x)  # right
         y = max(-(self.height - HEIGHT), y)  # bottom
-        #print(x,y)
+        # print(x,y)
         self.camera = pygame.Rect(x, y, self.width, self.height)
+
 
 pygame.quit()
